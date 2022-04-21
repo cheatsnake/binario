e3a7c630b3a1415dae0100bb6732d99273fee993
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-04-21 23:22:17 +0300

    add new constants

--- src/constants/error.constants.ts
+++ src/constants/error.constants.ts
@@ -1,2 +1,7 @@
 export const FIELD_NOT_GENERATED =
-    "To call this method, a field must be generated.";
+    "To call this method, a field must be generated";
+export const DUPLICATION_ERROR = "All columns and rows must be unique";
+export const BALANCE_ERROR =
+    "Tiles of different colors should be in equal quantity on each row and column";
+export const TRIPLE_ERROR =
+    "No more than two consecutive tiles of the same color are allowed";
