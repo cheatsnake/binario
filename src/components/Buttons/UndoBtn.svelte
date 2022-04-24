9f2584d979efcbf64d52f4f3872fcfe9c6470747
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-04-24 09:35:42 +0300

    add undo button

---
+++ src/components/Buttons/UndoBtn.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import UndoIcon from '../Icons/UndoIcon.svelte';
+    
+    export let undo: Function;
+</script>
+
+<button class="undo" on:click="{() => undo()}"><UndoIcon /></button>
+
+<style lang="scss">
+    .undo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        border: none;
+        background: none;
+        text-align: center;
+        font-size: 1.5rem;
+        font-weight: 400;
+        color: #2ddaff;
+        transition: 0.3s all;
+        &:hover {
+            transform: scale(1.2);
+        }
+        &:active {
+            background: none;
+            opacity: 0.3;
+        } 
+    }
+</style>
