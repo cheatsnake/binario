352cc0b852019b14cb7d215cab9f3159a895404c
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-04-25 21:25:24 +0300

    update styles and swap logic

--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -6,30 +6,35 @@
     export let value: string;
     export let id: string;
     export let changeTile: (r: number, c: number, v: TileValues) => void;
-    let tile: HTMLDivElement
-    const tileStyle = "tile"
-    const blockedTile = "tile_block"
+    let tile: HTMLDivElement;
+    const tileStyle = "tile";
+    const blockedTile = "tile_block";
 
-    function swapTile() {
-        const row = +tile.id.split('-')[0]
-        const column = +tile.id.split('-')[1]
+    function swapTile(primary: TileValues, secondary: TileValues) {
+        const row = +tile.id.split('-')[0];
+        const column = +tile.id.split('-')[1];
 
-        if (tile.classList.contains("tile_1")) {
-            tile.classList.remove("tile_1")
-            tile.classList.add("tile_0")
-            changeTile(row, column, TileValues.ZERO)
-        } else if (tile.classList.contains("tile_0")) {
-            tile.classList.remove("tile_0")
-            changeTile(row, column, TileValues.EMPTY)
+        if (tile.classList.contains(`tile_${primary}`)) {
+            tile.classList.remove(`tile_${primary}`);
+            tile.classList.add(`tile_${secondary}`);
+            changeTile(row, column, secondary);
+        } else if (tile.classList.contains(`tile_${secondary}`)) {
+            tile.classList.remove(`tile_${secondary}`);
+            changeTile(row, column, TileValues.EMPTY);
         } else {
-            tile.classList.add("tile_1")
-            changeTile(row, column, TileValues.ONE)
+            tile.classList.add(`tile_${primary}`);
+            changeTile(row, column, primary);
         }
     }
 
     onMount(() => {
         tile.addEventListener('click', () => {
-            if (!tile.classList.contains(blockedTile)) swapTile()
+            if (!tile.classList.contains(blockedTile)) 
+                swapTile(TileValues.ONE, TileValues.ZERO);
+        })
+        tile.addEventListener('contextmenu', () => {
+            if (!tile.classList.contains(blockedTile)) 
+                swapTile(TileValues.ZERO, TileValues.ONE);
         })
     })
     
@@ -47,32 +52,41 @@
 {/if}
 
 <style lang="scss">
+    @keyframes error {
+        0%    { border: 0.3rem solid rgba(255, 255, 255, 1) }
+        50%   { border: 0.3rem solid rgba(255, 255, 255, 0.5) }
+        100%  { border: 0.3rem solid rgba(255, 255, 255, 1) }
+    }
     .tile {
         user-select: none;
-        border-radius: 0.5rem;
-        background-color: #171a20;
+        border-radius: 0.4rem;
+        background-color: #161b22;
         width: 100%;
         height: 100%;
         transition: 0.15s all;
         &_1 {   
             background-color: #ffb33b;
-            box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, 
-            rgba(0, 0, 0, 0.3) 0px 7px 3px -3px, 
-            rgba(0, 0, 0, 0.25) 0px -0.5rem 0px inset;
         }
         &_0 {
             background-color: #2ddaff;
+        }
+        &_1, &_0 {
             box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, 
             rgba(0, 0, 0, 0.3) 0px 7px 3px -3px, 
             rgba(0, 0, 0, 0.25) 0px -0.5rem 0px inset;
+            &:hover {
+                transform: scale(1.05);
+            }
         }
         &_block {
             font-size: 2rem;
             text-align: center;
             color: rgba(23, 26, 32, 0.4);
         }
-        &:hover {
-            transform: scale(1.05);
+        &_error {
+            animation-name: error;
+            animation-duration: 1.5s;
+            animation-iteration-count: infinite;
         }
     }
 </style>
