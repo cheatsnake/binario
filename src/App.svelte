095b58097730e1191366ad9d40fc3e4a8ee2edbc
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-04-26 21:44:58 +0300

    add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+	import { Route, path } from "svelte-micro";
+	import { fade } from 'svelte/transition';
 	import GamePage from './pages/GamePage.svelte';
+	import MenuPage from './pages/MenuPage.svelte';
 </script>
-
-<div class="app">
-	<GamePage />
+  
+<div transition:fade={{duration: 500}} class="app">
+<Route>
+	<!-- <div transition:fade={{duration: 500}} class="app"> -->
+		<Route path="/4"><GamePage size="{4}" /></Route>
+		<Route path="/6"><GamePage size="{6}" /></Route>
+		<Route path="/8"><GamePage size="{8}" /></Route>
+		<Route path="/10"><GamePage size="{10}" /></Route>
+		<Route path="/12"><GamePage size="{12}" /></Route>
+		<Route path="/"><MenuPage /></Route>
+	<!-- </div> -->
+</Route>
 </div>
 
 <style lang="scss">
@@ -17,5 +29,7 @@
 		align-items: center;
 		background-color: #0d1117;
 		color: #2ddaff;
+		z-index: -2;
+		overflow: hidden;
 	}
 </style>
